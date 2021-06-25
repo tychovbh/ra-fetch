8633a3a72e05967ee82dac49c09101681cb8aa30
Tycho <info@bespokeweb.nl>
2021-06-25 11:19:50 +0200

    add response data like errors to update/store states

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ class Config {
     const submit = (submitParams = {}) => {
       setStore({...store, loading: true})
       return this.Request.store(name, {...store.data, ...submitParams}).then(response => {
-        setStore({...store, data: model, loading: false})
+        setStore({...store, ...response, data: model, loading: false})
 
         if (this.setRecords && response.records) {
           this.setRecords(response.records)
@@ -95,7 +95,7 @@ class Config {
     const submit = (submitParams = {}) => {
       setUpdate({...update, loading: true})
       return this.Request.update(name, {...update.data, ...submitParams}).then(response => {
-        setUpdate({...update, loading: false})
+        setUpdate({...update, ...response, data: update.data, loading: false})
 
         if (this.setRecords && response.records) {
           this.setRecords(response.records)
