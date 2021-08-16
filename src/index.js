ab62c754d685171df24b113a3ae21957604d3575
Tycho <info@bespokeweb.nl>
2021-08-16 11:15:33 +0200

    update a fetch

--- src/index.js
+++ src/index.js
@@ -3,269 +3,269 @@ import {Fetcher, Router, Client} from 'a-fetch'
 import {get} from 'js-expansion'
 
 class Config {
-  constructor(api) {
-    this.Request = api ? Fetcher.api(api) : Fetcher.request()
-    this.setRecords = null
-    this._mapping = {}
-    this._data = null
-  }
-
-  index(name, params = {}) {
-    const [indexParams, setIndexParams] = useState({
-      params,
-      append: false,
-      skip: !!this._data,
-    })
-    const [index, setIndex] = useState({...Fetcher.collection(), data: this._data || Fetcher.collection().data})
-
-    useEffect(() => {
-      if (indexParams.skip) {
-        return
-      }
-
-      setIndex({...index, loading: true})
-      this.Request.index(name, indexParams.params).then(response => {
-        if (indexParams.append) {
-          response.data = index.data.concat(response.data)
-        }
-
-        setIndex(response)
-
-        if (this.setRecords && response.records) {
-          this.setRecords(response.records)
-        }
-      })
-
-    }, [indexParams.params])
-
-    return [
-      index,
-      data => setIndex({...index, data}),
-      indexParams.params,
-      (params, append = false) => setIndexParams({params, append, skip: false}),
-    ]
-  }
-
-  show(name, params = {}) {
-    const [showParams, setShowParams] = useState(params)
-    const [show, setShow] = useState(Fetcher.model())
-
-    useEffect(() => {
-      setShow({...show, loading: true})
-      this.Request.show(name, showParams).then(response => {
-        setShow(response)
-
-        if (this.setRecords && response.records) {
-          this.setRecords(response.records)
-        }
-      })
-    }, [showParams])
-
-    return [show, data => setShow({...show, data}), showParams, setShowParams]
-  }
-
-  store(name, model = {}) {
-    const [store, setStore] = useState({...Fetcher.model(), loading: false, data: model})
-
-    const submit = (submitParams = {}) => {
-      setStore({...store, loading: true})
-      return this.Request.store(name, {...store.data, ...submitParams}).then(response => {
-        setStore({...store, ...response, data: response.errors.length ? store.data : model, loading: false})
-
-        if (this.setRecords && response.records) {
-          this.setRecords(response.records)
-        }
-
-        return response
-      })
+    constructor(api) {
+        this.Request = api ? Fetcher.api(api) : Fetcher.request()
+        this.setRecords = null
+        this._mapping = {}
+        this._data = null
     }
 
-    return [
-      store,
-      (data) => setStore({...store, data: {...store.data, ...data}}),
-      submit,
-    ]
-  }
-
-  update(name, model = {}, params = {}) {
-    const [update, setUpdate] = useState({...Fetcher.model(), loading: false, data: model})
-    const updateData = (data) => setUpdate({...update, loading: false, data: {...update.data, ...data}})
-
-    if (Object.keys(params).length) {
-      useEffect(() => {
-        setUpdate({...update, loading: true})
-        this.Request.show(name, params).then(response => {
-          const data = {}
-
-          Object.keys(model).map((name) => {
-            if (this._mapping[name]) {
-              if (typeof this._mapping[name] === 'function') {
-                data[name] = this._mapping[name](response.data)
-              } else {
-                data[name] = get(response.data, this._mapping[name]) || ''
-              }
-            } else if (response.data.hasOwnProperty(name)) {
-              data[name] = response.data[name]
-            }
-          })
-
-          updateData(data)
+    index(name, params = {}) {
+        const [indexParams, setIndexParams] = useState({
+            params,
+            append: false,
+            skip: !!this._data,
         })
-      }, [])
+        const [index, setIndex] = useState({...Fetcher.collection(), data: this._data || Fetcher.collection().data})
+
+        useEffect(() => {
+            if (indexParams.skip) {
+                return
+            }
+
+            setIndex({...index, loading: true})
+            this.Request.index(name, indexParams.params).then(response => {
+                if (indexParams.append) {
+                    response.data = index.data.concat(response.data)
+                }
+
+                setIndex(response)
+
+                if (this.setRecords && response.records) {
+                    this.setRecords(response.records)
+                }
+            })
+
+        }, [indexParams.params])
+
+        return [
+            index,
+            data => setIndex({...index, data}),
+            indexParams.params,
+            (params, append = false) => setIndexParams({params, append, skip: false}),
+        ]
     }
 
-    const submit = (submitParams = {}) => {
-      setUpdate({...update, loading: true})
-      return this.Request.update(name, {...update.data, ...submitParams}).then(response => {
-        setUpdate({...update, ...response, data: update.data, loading: false})
+    show(name, params = {}) {
+        const [showParams, setShowParams] = useState(params)
+        const [show, setShow] = useState(Fetcher.model())
 
-        if (this.setRecords && response.records) {
-          this.setRecords(response.records)
+        useEffect(() => {
+            setShow({...show, loading: true})
+            this.Request.show(name, showParams).then(response => {
+                setShow(response)
+
+                if (this.setRecords && response.records) {
+                    this.setRecords(response.records)
+                }
+            })
+        }, [showParams])
+
+        return [show, data => setShow({...show, data}), showParams, setShowParams]
+    }
+
+    store(name, model = {}) {
+        const [store, setStore] = useState({...Fetcher.model(), loading: false, data: model})
+
+        const submit = (submitParams = {}) => {
+            setStore({...store, loading: true})
+            return this.Request.store(name, {...store.data, ...submitParams}).then(response => {
+                setStore({...store, ...response, data: response.errors.length ? store.data : model, loading: false})
+
+                if (this.setRecords && response.records) {
+                    this.setRecords(response.records)
+                }
+
+                return response
+            })
         }
 
-        return response
-      })
+        return [
+            store,
+            (data) => setStore({...store, data: {...store.data, ...data}}),
+            submit,
+        ]
     }
 
-    return [
-      update,
-      updateData,
-      submit,
-    ]
-  }
+    update(name, model = {}, params = {}) {
+        const [update, setUpdate] = useState({...Fetcher.model(), loading: false, data: model})
+        const updateData = (data) => setUpdate({...update, loading: false, data: {...update.data, ...data}})
 
-  delete(name, params = {}) {
-    const [destroy, setDestroy] = useState({loading: false})
+        if (Object.keys(params).length) {
+            useEffect(() => {
+                setUpdate({...update, loading: true})
+                this.Request.show(name, params).then(response => {
+                    const data = {}
 
-    const submit = (submitParams = {}) => {
-      setDestroy({loading: true})
-      return this.Request.delete(name, {...params, ...submitParams}).then(response => {
-        setDestroy({loading: false})
+                    Object.keys(model).map((name) => {
+                        if (this._mapping[name]) {
+                            if (typeof this._mapping[name] === 'function') {
+                                data[name] = this._mapping[name](response.data)
+                            } else {
+                                data[name] = get(response.data, this._mapping[name]) || ''
+                            }
+                        } else if (response.data.hasOwnProperty(name)) {
+                            data[name] = response.data[name]
+                        }
+                    })
 
-        if (this.setRecords && response.records) {
-          this.setRecords(response.records)
+                    updateData(data)
+                })
+            }, [])
         }
 
-        return response
-      })
+        const submit = (submitParams = {}) => {
+            setUpdate({...update, loading: true})
+            return this.Request.update(name, {...update.data, ...submitParams}).then(response => {
+                setUpdate({...update, ...response, data: update.data, loading: false})
+
+                if (this.setRecords && response.records) {
+                    this.setRecords(response.records)
+                }
+
+                return response
+            })
+        }
+
+        return [
+            update,
+            updateData,
+            submit,
+        ]
     }
 
-    return [
-      destroy,
-      submit,
-    ]
-  }
+    delete(name, params = {}) {
+        const [destroy, setDestroy] = useState({loading: false})
 
-  login(model) {
-    const [login, setLogin] = useState({...Fetcher.model(), loading: false, data: model})
+        const submit = (submitParams = {}) => {
+            setDestroy({loading: true})
+            return this.Request.delete(name, {...params, ...submitParams}).then(response => {
+                setDestroy({loading: false})
 
-    const submit = () => {
-      setLogin({...login, loading: true})
-      return this.Request.login(login.data).then(response => {
-        const data = response.errors.length ? login.data : model
-        setLogin({...login, ...response, data, loading: false})
-        return response
-      })
+                if (this.setRecords && response.records) {
+                    this.setRecords(response.records)
+                }
+
+                return response
+            })
+        }
+
+        return [
+            destroy,
+            submit,
+        ]
     }
 
-    return [
-      login,
-      (data) => setLogin({...login, data: {...login.data, ...data}}),
-      submit,
-    ]
-  }
+    login(model) {
+        const [login, setLogin] = useState({...Fetcher.model(), loading: false, data: model})
 
-  logout(model) {
-    const [logout, setLogout] = useState({...Fetcher.model(), loading: false, data: model})
+        const submit = () => {
+            setLogin({...login, loading: true})
+            return this.Request.login(login.data).then(response => {
+                const data = response.errors.length ? login.data : model
+                setLogin({...login, ...response, data, loading: false})
+                return response
+            })
+        }
 
-    const submit = () => {
-      setLogout({...logout, loading: true})
-      return this.Request.logout(logout.data).then(response => {
-        setLogout({...logout, ...response, data: model, loading: false})
-        return response
-      })
+        return [
+            login,
+            (data) => setLogin({...login, data: {...login.data, ...data}}),
+            submit,
+        ]
     }
 
-    return [
-      logout,
-      (data) => setLogout({...logout, data: {...logout.data, ...data}}),
-      submit,
-    ]
-  }
+    logout(model) {
+        const [logout, setLogout] = useState({...Fetcher.model(), loading: false, data: model})
 
-  data(data) {
-    this._data = data
-    return this
-  }
+        const submit = () => {
+            setLogout({...logout, loading: true})
+            return this.Request.logout(logout.data).then(response => {
+                setLogout({...logout, ...response, data: model, loading: false})
+                return response
+            })
+        }
 
-  records(setRecords = null, records = [], key = 'id') {
-    this.setRecords = setRecords
-    this.Request.records(records, key)
-    return this
-  }
+        return [
+            logout,
+            (data) => setLogout({...logout, data: {...logout.data, ...data}}),
+            submit,
+        ]
+    }
 
-  mapping(mapping = {}) {
-    this._mapping = mapping
-    return this
-  }
+    data(data) {
+        this._data = data
+        return this
+    }
 
-  api(api) {
-    this.Request.api(api)
-    return this
-  }
+    records(setRecords = null, records = [], key = 'id') {
+        this.setRecords = setRecords
+        this.Request.records(records, key)
+        return this
+    }
 
-  bearerToken(bearer_token) {
-    this.Request.bearerToken(bearer_token)
-    return this
-  }
+    mapping(mapping = {}) {
+        this._mapping = mapping
+        return this
+    }
+
+    api(api) {
+        this.Request.api(api)
+        return this
+    }
+
+    bearerToken(bearer_token) {
+        this.Request.bearerToken(bearer_token)
+        return this
+    }
 }
 
 export const useApi = (name) => {
-  return new Config(name)
+    return new Config(name)
 }
 
 export const useBearerToken = (name) => {
-  return new Config().bearerToken(name)
+    return new Config().bearerToken(name)
 }
 
 export const useRecords = (setRecords = null, records = [], key = 'id') => {
-  return new Config().records(setRecords, records, key)
+    return new Config().records(setRecords, records, key)
 }
 
 export const useData = (data) => {
-  return new Config().data(data)
+    return new Config().data(data)
 }
 
 export const useMapping = (mapping = {}) => {
-  return new Config().mapping(mapping)
+    return new Config().mapping(mapping)
 }
 
 export const useIndex = (name, params = {}) => {
-  return new Config().index(name, params)
+    return new Config().index(name, params)
 }
 
 export const useShow = (name, params = {}) => {
-  return new Config().show(name, params)
+    return new Config().show(name, params)
 }
 
 export const useStore = (name, model = {}) => {
-  return new Config().store(name, model)
+    return new Config().store(name, model)
 }
 
 export const useUpdate = (name, model = {}, params = {}) => {
-  return new Config().update(name, model, params)
+    return new Config().update(name, model, params)
 }
 
 export const useDelete = (name, params = {}) => {
-  return new Config().delete(name, params)
+    return new Config().delete(name, params)
 }
 
 export const useLogin = (model = {}) => {
-  return new Config().login(model)
+    return new Config().login(model)
 }
 
 export const useLogout = (model = {}) => {
-  return new Config().logout(model)
+    return new Config().logout(model)
 }
 
 export {Fetcher, Router, Client}
